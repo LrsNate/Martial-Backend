b5cc29d1beabb67f3a981577897b5680fd8d992a
Antoine Lafouasse <lafouaa@amazon.com>
2016-11-18 13:33:32 +0000

    Setup Redis

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const redis = require('redis');
 
 const app = express();
+const redisClient = redis.createClient();
 
 app.get('/', (req, res) => {
   res.sendStatus(204);
+});
+
+app.get('/ping', (req, res) => {
+  redisClient.ping(() => {
+    res.sendStatus(202);
+  });
+});
+
+app.get('/works/latest', (req, res) => {
+  redisClient.get('works:versions:latest', (err, result) => {
+    res.send(result);
+  });
 });
 
 app.listen(3000, () => {
