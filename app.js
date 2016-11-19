174a116813f5451f4b3d810c3dfd30492d32be7b
Antoine Lafouasse <lafouaa@amazon.com>
2016-11-19 13:42:12 +0000

    Promisify redis client

--- app.js
+++ app.js
@@ -1,25 +1,42 @@
 const express = require('express');
 const redis = require('redis');
+const bluebird = require('bluebird');
 
 const app = express();
-const redisClient = redis.createClient();
+bluebird.promisifyAll(redis.RedisClient.prototype);
 
-app.get('/', (req, res) => {
-  res.sendStatus(204);
-});
+function getRedisClient() {
+  return redis.createClient();
+}
 
-app.get('/ping', (req, res) => {
-  redisClient.ping(() => {
-    res.sendStatus(202);
+app.get('/works/latest', (req, res) => {
+  const redisClient = getRedisClient();
+  redisClient.getAsync('works:versions:latest').then((latestVersion) => { // eslint-disable-line arrow-body-style
+    return redisClient.getAsync(`works:version:${latestVersion}`);
+  }).then((dump) => {
+    res.send(dump);
   });
 });
 
-app.get('/works/latest', (req, res) => {
-  redisClient.get('works:versions:latest', (err, result) => {
-    res.send(result);
+app.get('/works/versions', (req, res) => {
+  const redisClient = getRedisClient();
+  redisClient.lrangeAsync('works:versions:all', 0, -1).then((list) => {
+    res.send(list);
+  });
+});
+
+app.get('/works/versions/:id', (req, res) => {
+  res.send(404);
+});
+
+app.put('/works/version', (req, res) => {
+  const redisClient = getRedisClient();
+  const body = null;
+  redisClient.set('key', body, () => {
+    res.send(201);
   });
 });
 
 app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 3000!'); // eslint-disable-line no-console
 });
